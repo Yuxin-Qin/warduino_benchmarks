a81ad046d3d8a8d4d94ceb03529f6030d85e36e1
Yuxin-Qin <y.qin.1@research.gla.ac.uk>
2025-08-05 03:16:10 +0100

    Modified nbode

--- nbody.c
+++ nbody.c
@@ -254,6 +254,8 @@ void start() {
     print_string("mian start\n", 11);
     offset_momentum(solar_bodies, BODIES_SIZE);
     print_string("test point 1\n", 13);
+    print_int((int)solar_bodies);
+    print_int((int)BODIES_SIZE);
     print_int((int)bodies_energy(solar_bodies, BODIES_SIZE));
     print_string("test point 2\n", 13);
     for (int i = 0; i < Z; ++i)
