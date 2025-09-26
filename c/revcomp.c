ea933c259cad3fc9f5b647999d17c1c76bb6eef4
Yuxin-Qin <y.qin.1@research.gla.ac.uk>
2025-09-26 21:28:07 +0100

    Updated revcomp.wasm

--- c/revcomp.c
+++ c/revcomp.c
@@ -6,7 +6,7 @@
 // Enhanced memory pool configuration for large sequences
 #define CELL_SIZE 64
 #define POOL_SIZE_IN_PAGES 64  // 256KB total memory
-#define PAGE_SIZE 4096
+#define PAGE_SIZE 256
 #define MAX_SEQUENCE_LENGTH 100000  // Process sequences in chunks
 #define MAX_LINE_LENGTH 80
 
