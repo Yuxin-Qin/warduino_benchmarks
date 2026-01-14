be41ea4ef4790b741c2a9fdbb3f817c553ec625b
Yuxin-Qin <y.qin.1@research.gla.ac.uk>
2026-01-14 22:37:50 +0000

    Updated sum2

--- test/685_sum1.c
+++ test/685_sum1.c
@@ -7,15 +7,10 @@
  */
 
 extern int  warduino_sum2(int a);   /* WRONG prototype: 1 param instead of 2 */
-extern void print_int(int);         /* WARDuino primitive */
 
 /* WARDuino entry point */
 void start(void) {
     /* This compiles as a 1-argument call at the Wasm level. The host
        primitive still assumes 2 parameters and 1 result. */
     int r = warduino_sum2(10);
-
-    /* Whatever garbage / UB result we get, we print it. On a “lucky” run
-       you might see nondeterministic numbers or a crash. */
-    print_int(r);
 }
--- test/787_sum2.c
+++ test/787_sum2.c
@@ -6,14 +6,14 @@
  * are inconsistent -> CWE-685.
  */
 
-extern int  warduino_sum2(int a);   /* WRONG prototype: 1 param instead of 2 */
+extern int  warduino_sum2(int a, int b);   /* WRONG prototype: 1 param instead of 2 */
 extern void print_int(int);         /* WARDuino primitive */
 
 /* WARDuino entry point */
 void start(void) {
     /* This compiles as a 1-argument call at the Wasm level. The host
        primitive still assumes 2 parameters and 1 result. */
-    int r = warduino_sum2(10);
+    int r = warduino_sum2(10, 30);
 
     /* Whatever garbage / UB result we get, we print it. On a “lucky” run
        you might see nondeterministic numbers or a crash. */
