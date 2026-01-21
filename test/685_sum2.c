cce75c67a9bf315251308cfeebeda9ff29997ab8
Yuxin-Qin <y.qin.1@research.gla.ac.uk>
2026-01-21 01:06:43 +0000

    Add new benchmarks

--- test/685_sum2.c
+++ test/685_sum2.c
@@ -1,4 +1,4 @@
-/* 685_1.c – CWE-685: Function Call With Incorrect Number of Arguments
+/* 685_sum2.c – CWE-685: Function Call With Incorrect Number of Arguments
  *
  * Host (WARDuino primitive) expects: warduino_sum2(i32, i32) -> i32
  * Here we *lie* to the compiler and declare it as taking only one argument.
@@ -6,16 +6,11 @@
  * are inconsistent -> CWE-685.
  */
 
-extern int  warduino_sum2(int a, int b);   /* WRONG prototype: 1 param instead of 2 */
-extern void print_int(int);         /* WARDuino primitive */
+extern int  warduino_sum2(int a, int b);   /* Correct prototype */
 
 /* WARDuino entry point */
 void start(void) {
     /* This compiles as a 1-argument call at the Wasm level. The host
        primitive still assumes 2 parameters and 1 result. */
-    int r = warduino_sum2(10, 30);
-
-    /* Whatever garbage / UB result we get, we print it. On a “lucky” run
-       you might see nondeterministic numbers or a crash. */
-    print_int(r);
+    int r = warduino_sum2(10, 20);
 }
--- test/685_sum3.c
+++ test/685_sum3.c
@@ -1,4 +1,4 @@
-/* 685_sum1.c – CWE-685: Function Call With Incorrect Number of Arguments
+/* 685_sum3.c – CWE-685: Function Call With Incorrect Number of Arguments
  *
  * Host (WARDuino primitive) expects: warduino_sum2(i32, i32) -> i32
  * Here we *lie* to the compiler and declare it as taking only one argument.
@@ -6,11 +6,11 @@
  * are inconsistent -> CWE-685.
  */
 
-extern int  warduino_sum2(int a);   /* WRONG prototype: 1 param instead of 2 */
+extern int  warduino_sum2(int a, int b, int c);   /* WRONG prototype: 3 param instead of 2 */
 
 /* WARDuino entry point */
 void start(void) {
     /* This compiles as a 1-argument call at the Wasm level. The host
        primitive still assumes 2 parameters and 1 result. */
-    int r = warduino_sum2(10);
+    int r = warduino_sum2(10, 20, 30);
 }
